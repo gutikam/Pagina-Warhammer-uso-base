a7f77a9df9a9e4514f6467e0485ce51d9cecec44
gutikam <gutikamfight@gmail.com>
2024-05-29 10:51:09 -0400

    cambios hechos

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
+//js del efecto netflix
+document.querySelectorAll(".image-card").forEach(card => {
+    card.addEventListener("mouseover", () => {
+        card.classList.add("hover");
+    });
 
+    card.addEventListener("mouseout", () => {
+        card.classList.remove("hover");
+    });
+});
 
